c4e6c0ec91e44f5325390d3ea9880a86d0c7d2bd
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-08-17 15:22:26 +0200

    fix path in logo

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -4,7 +4,7 @@ import "./topbar.scss";
 const Topbar: React.FC = () => {
   return (
     <div className="topbar">
-      <a className="topbar__link" href="/" rel="noopener noreferrer">
+      <a className="topbar__link" href="/aic-exhibitions/" rel="noopener noreferrer">
         <h1 className="topbar__link-text">The Art Institute of Chicago - Exhibitions</h1>
       </a>
     </div>
